5b4f36edc7649e0a0b563d2f442a205bf3e5f776
rr- <rr-@sakuya.pl>
2020-03-11 18:56:10 +0100

    clean: add tests

---
+++ scripts/clean/test_command.py
@@ -0,0 +1,37 @@
+import re
+import typing as T
+
+import pytest
+
+from .process import (
+    fix_bad_dialogue_dashes,
+    fix_dangling_ass_tags,
+    fix_disjoint_ass_tags,
+    fix_punctuation,
+    fix_whitespace,
+)
+
+
+def test_fix_disjointed_ass_tags() -> None:
+    assert fix_disjoint_ass_tags("{asd}{asd}") == "{asdasd}"
+
+
+def test_fix_dangling_ass_tags() -> None:
+    assert fix_dangling_ass_tags("{\\i1}asd{\\i0}") == "{\\i1}asd"
+
+
+def test_fix_bad_dialogue_dashes() -> None:
+    assert (
+        fix_bad_dialogue_dashes("- asd\n- asd")
+        == "\N{EN DASH} asd\n\N{EN DASH} asd"
+    )
+
+
+def test_fix_whitespace() -> None:
+    assert fix_whitespace(" asd ") == "asd"
+    assert fix_whitespace("asd \nasd") == "asd\nasd"
+    assert fix_whitespace("asd\n asd") == "asd\nasd"
+
+
+def test_punctuation() -> None:
+    assert fix_punctuation("asd...") == "asd\N{HORIZONTAL ELLIPSIS}"
