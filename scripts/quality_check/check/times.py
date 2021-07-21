e25195198eef2a74dfabb2b228e3c1c722fe4b27
rr- <rr-@sakuya.pl>
2021-07-21 18:32:42 +0200

    qc/times: improve support for bad interleaving

--- scripts/quality_check/check/times.py
+++ scripts/quality_check/check/times.py
@@ -9,11 +9,16 @@ from .base import BaseEventCheck, BaseResult, Violation
 
 
 class CheckTimes(BaseEventCheck):
-    WIDTH = 64
-    HEIGHT = 48
+    """This check verifies if the subtitles snap to scene boundaries up to
+    `MAX_DISTANCE` frames. A scene boundary is understood to be when the camera
+    in the video changes from one scene to another.
+    """
+
+    WIDTH = 4
+    HEIGHT = 3
     MAX_DISTANCE = 2
     MIN_RGB_DELTA = 3
-    FRAME_CACHE: T.Dict[T.Tuple[str, int], np.float] = {}
+    FRAME_CACHE: T.Dict[T.Tuple[str, int], float] = {}
     SNAP_CACHE: T.Dict[T.Tuple[str, int], bool] = {}
 
     async def run_for_event(self, event: AssEvent) -> T.Iterable[BaseResult]:
@@ -25,14 +30,14 @@ class CheckTimes(BaseEventCheck):
         if not await self.video_frame_snaps(event.end):
             yield Violation(event, "end does not snap to scene boundary")
 
-    async def get_video_frame_avg(self, frame_idx: int) -> np.float:
+    async def get_video_frame_avg(self, frame_idx: int) -> float:
         cache_key = (self.api.video.current_stream.path, frame_idx)
         if cache_key not in self.FRAME_CACHE:
-            self.FRAME_CACHE[cache_key] = np.mean(
+            self.FRAME_CACHE[cache_key] = (
                 await self.api.video.current_stream.async_get_frame(
                     frame_idx, self.WIDTH, self.HEIGHT
                 )
-            )
+            ).astype(np.int16)
         return self.FRAME_CACHE[cache_key]
 
     async def video_frame_snaps(self, pts: int) -> bool:
@@ -47,12 +52,20 @@ class CheckTimes(BaseEventCheck):
                 )
 
             prev = frame_data[-self.MAX_DISTANCE]
-            pivot = None
+            pivots: T.List[T.Tuple[int, float]] = []
             for delta in range(-self.MAX_DISTANCE + 1, self.MAX_DISTANCE + 1):
                 current = frame_data[delta]
-                if abs(current - prev) > self.MIN_RGB_DELTA:
-                    pivot = delta
+                diff = np.mean(np.abs(current - prev))
+                if diff > self.MIN_RGB_DELTA:
+                    pivots.append((delta, diff))
                 prev = current
 
-            self.SNAP_CACHE[cache_key] = pivot is None or pivot == 0
+            if pivots:
+                best_pivot, best_diff = max(
+                    sorted(pivots), key=lambda kv: kv[1]
+                )
+            else:
+                best_pivot, best_diff = None, None
+
+            self.SNAP_CACHE[cache_key] = best_pivot is None or best_pivot == 0
         return self.SNAP_CACHE[cache_key]
