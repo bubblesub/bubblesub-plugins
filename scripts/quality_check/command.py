71aadb9213a55b97417ddd5ada5e77bcf824dd4c
rr- <dash@wind.garden>
2021-12-13 12:34:04 +0100

    qc: add --full

--- scripts/quality_check/command.py
+++ scripts/quality_check/command.py
@@ -1,6 +1,6 @@
 import argparse
 import bisect
-import typing as T
+from collections.abc import Iterable
 
 from bubblesub.api import Api
 from bubblesub.api.cmd import BaseCommand
@@ -9,6 +9,7 @@ from bubblesub.ass_renderer import AssRenderer
 
 from .check import (
     BaseResult,
+    BaseCheck,
     CheckActorStats,
     CheckAssTags,
     CheckDoubleWords,
@@ -30,25 +31,40 @@ from .check.fonts import get_fonts
 from .common import benchmark, get_video_height, get_video_width
 
 
-async def list_violations(api: Api) -> T.Iterable[BaseResult]:
+def get_event_checks(full: bool) -> Iterable[BaseCheck]:
+    yield CheckStyleValidity
+    yield CheckAssTags
+    yield CheckDurations
+    yield CheckPunctuation
+    yield CheckQuotes
+    yield CheckLineContinuation
+    yield CheckDoubleWords
+    yield CheckUnnecessaryBreaks
+    yield CheckLongLines
+
+    if full:
+        yield CheckTimes
+
+
+def get_global_checks(full: bool) -> Iterable[BaseCheck]:
+    yield CheckVideoResolution
+    yield CheckSpelling
+    yield CheckActorStats
+    yield CheckStyleStats
+    yield CheckFonts
+    yield CheckPunctuationStats
+
+
+async def list_violations(
+    api: Api, checks: Iterable[BaseCheck]
+) -> Iterable[BaseResult]:
     renderer = AssRenderer()
     renderer.set_source(
         ass_file=api.subs.ass_file,
         video_resolution=(get_video_width(api), get_video_height(api)),
     )
 
-    for check_cls in [
-        CheckStyleValidity,
-        CheckAssTags,
-        CheckDurations,
-        CheckPunctuation,
-        CheckQuotes,
-        CheckLineContinuation,
-        CheckDoubleWords,
-        CheckUnnecessaryBreaks,
-        CheckLongLines,
-        CheckTimes,
-    ]:
+    for check_cls in checks:
         with benchmark(api, f"{check_cls}"):
             check = check_cls(api, renderer)
             async for violation in check.get_violations():
@@ -69,15 +85,24 @@ class QualityCheckCommand(BaseCommand):
             dest="clear_cache",
             help="clear font cache",
         )
+        parser.add_argument(
+            "-f",
+            "--full",
+            action="store_true",
+            help="run slower checks",
+        )
 
     async def run(self):
         if self.args.clear_cache:
             get_fonts.cache_clear()
 
+        event_checks = get_event_checks(full=self.args.full)
+        global_checks = get_global_checks(full=self.args.full)
+
         if self.args.focus_prev or self.args.focus_next:
             violations = [
                 result
-                async for result in list_violations(self.api)
+                async for result in list_violations(self.api, event_checks)
                 if result.log_level in {LogLevel.WARNING, LogLevel.ERROR}
             ]
             if not violations:
@@ -108,17 +133,10 @@ class QualityCheckCommand(BaseCommand):
                     self.api.log.log(result.log_level, repr(result))
             return
 
-        async for result in list_violations(self.api):
+        async for result in list_violations(self.api, event_checks):
             self.api.log.log(result.log_level, repr(result))
 
-        for check_cls in [
-            CheckVideoResolution,
-            CheckSpelling,
-            CheckActorStats,
-            CheckStyleStats,
-            CheckFonts,
-            CheckPunctuationStats,
-        ]:
+        for check_cls in global_checks:
             with benchmark(self.api, f"{check_cls}"):
                 check = check_cls(self.api)
                 await check.run()
